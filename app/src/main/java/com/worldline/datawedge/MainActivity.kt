0a29a3b0ace921e122e0e616e0fd7c00f7933fbf
Sergio Casero hernández <sergio.casero23@gmail.com>
2018-01-13 12:50:55 +0100

    Added dataWedge to activity

--- app/src/main/java/com/worldline/datawedge/MainActivity.kt
+++ app/src/main/java/com/worldline/datawedge/MainActivity.kt
@@ -1,12 +1,38 @@
 package com.worldline.datawedge
 
-import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.support.v7.app.AppCompatActivity
+import com.worldline.datawedge_barcode_scanner.DataWedge
+import com.worldline.datawedge_barcode_scanner.ScanData
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
+
+    private val dataWedge: DataWedge by lazy { DataWedge(context = this, callback = { handleScanData(it) }) }
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        dataWedge.initialize()
+
+        registerListeners()
     }
+
+    override fun onDestroy() {
+        super.onDestroy()
+
+        dataWedge.destroy()
+    }
+
+    private fun registerListeners() {
+
+    }
+
+    private fun handleScanData(scanData: ScanData) {
+        decodedBy.text = scanData.decodedBy
+        code.text = scanData.code
+        label.text = scanData.labelType
+    }
+
 }
